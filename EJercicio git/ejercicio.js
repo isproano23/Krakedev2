7dea2fb6675b8ad64a1ef62cb981dc56a0eb0992
Isaac <sebasproano917@gmail.com>
2025-09-22 17:35:08 -0500

    se agrega un gif a la pagina

--- EJercicio git/ejercicio.js
+++ EJercicio git/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let  cmpImagenSaludo
+    cmpImagenSaludo=document.getElementById("imgsaludo")
+    cmpImagenSaludo.src="./imagenes/homer.gif"
     console.log("Ingrese a saludar")
 }
