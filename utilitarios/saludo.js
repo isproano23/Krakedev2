8a46fbad3bb842c2ceed1999608be0e9ce2bdb79
Isaac <sebasproano917@gmail.com>
2025-09-23 18:23:25 -0500

    se separa las funciones al archivo .js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -14,35 +14,3 @@ saludar = function () {
 };
 
 
-mostrarTextoCaja=function(idcomponente,mensaje){
-  let componente = document.getElementById(idcomponente);
-  componente.value = mensaje;
-}
-
-recuperarTexto = function (idcomponente) {
-  let componente;
-  let valorIngresado;
-  componente = document.getElementById(idcomponente);
-  valorIngresado = componente.value;
-  return valorIngresado;
-};
-
-mostrarTexto = function (idcomponente, mensaje) {
-  let componente = document.getElementById(idcomponente);
-  componente.innerText = mensaje;
-};
-recuperarInt = function (idcomponente) {
-  let valorCaja = recuperarTexto(idcomponente);
-  let valorEntero = parseInt(valorCaja);
-  return valorEntero;
-};
-recuperarFloat = function (idcomponente) {
-  let valorCaja = recuperarTexto(idcomponente);
-  let valorFloat = parseFloat(valorCaja);
-  return valorFloat;
-};
-mostrarImagen = function (idcomponente, rutaImagen) {
-  let componente;
-  componente = document.getElementById(idcomponente);
-  componente.src = rutaImagen;
-};
